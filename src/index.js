619b2329a50e34aaede87f68745479c9f7768978
ricklopez <ricklopez@outlook.com>
2018-08-12 12:08:19 -0400

    [Configure Dependencies]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import createSagaMiddleware from 'redux-saga';
+
+
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/esm/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
